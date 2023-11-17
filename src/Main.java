7bb93a9dbde92bb815db27fc252dde4ec666ec1b
kosty <round.mc@yandex.by>
2023-11-16 19:36:27 -0500

    Вот теперь 7 задание сделано)

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import java.util.Arrays;
 import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         int arr[] = {1, 4, 0, 5, 6, 3};
@@ -12,15 +14,26 @@ public class Main {
             System.out.print(arr[i] + " ");
         }
         System.out.println(" ");
+        int x = 1;
 
         int newarr2[] = new int[arr2.length * 2];
-        for (int i = 0; i < arr2.length; i++) {
-            newarr2[i * 2] = arr2[i];
-            newarr2[i * 2 + 1] = (arr2[i] + 1) % 10;
+        for (int i = arr2.length - 1; i >= 0; i--) {
+            int y = arr2[i] + x;
+            if (y == 10) {
+                arr2[i] = 0;
+            } else {
+                arr2[i] = y;
+                x = 0;
+                break;
+            }
         }
-
+        if (x == 1) {
+            newarr2[0] = 1;
+        }
         for (int i = 0; i < newarr2.length; i++) {
             System.out.print(newarr2[i] + " ");
         }
     }
 }
+
+
